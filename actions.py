a133ee860ee578c9c58c4de3ac592ae531b24dbd
Deepak Kumar Bhardwaj <deepak@adeptmind.ai>
2023-10-16 16:54:22 +0530

    add run return

--- actions.py
+++ actions.py
@@ -8,6 +8,7 @@ from django.urls import path
 from mysite.settings import SECRET_KEY
 from prefect import Flow, task
 
+
 def run_actions():
     print("##### CHECK FOR ENV VALUES")
     mms_url = os.environ["MMS_URL"]
@@ -29,6 +30,50 @@ def run_actions():
     }
     with open("sample_config.json", "w") as f:
         json.dump(data, f)
+
+    feeds_could_not_be_added = []
+    feeds_could_not_be_removed = []
+    smt_added_feeds = smt_removed_feeds = []
+    # generate logs for the feeds which couldn't be added and removed.
+    feeds_could_not_be_added_logs = (
+        f"{len(feeds_could_not_be_added)}/{len(smt_added_feeds)} feeds couldn't be added."
+        f" Here is the list : {_convert_feeds_to_display_format(feeds_could_not_be_added)}"
+    )
+    feeds_could_not_be_removed_logs = (
+        f"{len(feeds_could_not_be_removed)}/{len(smt_removed_feeds)} feeds couldn't be removed."
+        f" Here is the list : {_convert_feeds_to_display_format(feeds_could_not_be_removed)}"
+    )
+
+    # generate logs for the feeds which have been added and removed.
+    successfully_added_feeds = []
+    for feed in smt_added_feeds:
+        if feed not in feeds_could_not_be_added:
+            successfully_added_feeds.append(feed)
+
+    successfully_removed_feeds = []
+    for feed in smt_removed_feeds:
+        if feed not in feeds_could_not_be_removed:
+            successfully_removed_feeds.append(feed)
+    successfully_added_feeds_logs = (
+        f"{len(successfully_added_feeds)}/{len(smt_added_feeds)} feeds have been added."
+        f" Here is the list : {_convert_feeds_to_display_format(successfully_added_feeds)}"
+    )
+    successfully_removed_feeds_logs = (
+        f"{len(successfully_removed_feeds)}/{len(smt_removed_feeds)} feeds have been removed."
+        f" Here is the list : {_convert_feeds_to_display_format(successfully_removed_feeds)}"
+    )
+
+    integration_logs = '\n'.join(
+        [
+            "##########################################################",
+            feeds_could_not_be_added_logs,
+            feeds_could_not_be_removed_logs,
+            "##########################################################",
+            successfully_added_feeds_logs,
+            successfully_removed_feeds_logs
+        ]
+    )
+    return integration_logs
 
 
 if __name__ == '__main__':
