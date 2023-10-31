328e0952e1d01d70f2517970f7c01d31f60df50d
Deepak Kumar Bhardwaj <deepak@adeptmind.ai>
2023-10-31 09:23:43 +0530

    jellyfish int

--- scripts/cli/main.py
+++ scripts/cli/main.py
@@ -11,45 +11,6 @@ def script():
     pass
 
 
-# @script.command()
-# @click.option("-e", "--env", default="local", help="environment to use")
-# @k8s_option
-# def register(env: str, k8: bool):
-#     """
-#     Register direct inventory flows with Prefect
-#     """
-#     for project, store_data in get_project_to_store_id_inventory_map().items():
-#         for store_id in store_data:
-#             tags = [
-#                 TagValues.k8s,
-#                 TagValues.mall,
-#                 TagValues.direct_inventory,
-#                 TagValues.high_priority,
-#                 get_validate_client_tag(project),
-#             ]
-#             infra_overrides = {}
-#             if store_id in BIG_STORES:
-#                 tags.append(TagValues.big_store)
-#                 infra_overrides = Kubernetes.get_high_memory_overrides()
-# 
-#             deployment_name = f"direct-inventory-update-{project}-{store_id}"
-#             deployment_builder = DeploymentBuilder(
-#                 flow=direct_inventory_update,
-#                 deployment_name=deployment_name,
-#                 parameters={"project": project, "store_id": store_id},
-#                 tags=tags,
-#                 schedule=(CronSchedule(cron="0 */4 * * *", timezone="Canada/Eastern")),
-#                 work_queue_name=WorkQueues.mall_kube_jobs,
-#             )
-# 
-#             is_kube = env == "kube" or k8
-# 
-#             if is_kube:
-#                 apply_deployment(deployment_builder.kubernetes(infra_overrides=infra_overrides))
-#             else:
-#                 apply_deployment(deployment_builder.local())
-
-
 @script.command()
 @click.option("-ua", "--url_to_ag_id", required=True)
 def migrate_to_jellyfish(url_to_ag_id):
@@ -57,7 +18,7 @@ def migrate_to_jellyfish(url_to_ag_id):
     Run direct inventory for a specific store
     """
     print("url_to_ag_id----", url_to_ag_id)
-    # move_stores_to_jellyfish(url_to_ag_id)
+    move_stores_to_jellyfish(url_to_ag_id)
 
 
 if __name__ == "__main__":
--- scripts/move_stores_to_jellyfish.py
+++ scripts/move_stores_to_jellyfish.py
@@ -1,2 +1,11 @@
-def move_stores_to_jellyfish():
-    pass
+import yaml
+
+_path = "/Users/deepakkumar/Adeptmind/example.django22.mysite/scripts/jellyfish_domains.yaml"
+
+
+def move_stores_to_jellyfish(_imap):
+    _map = yaml.safe_load(_path)
+    output = _map | _imap
+    _map.update(_imap)
+    with open(_path, "w") as f:
+        yaml.dump(output, f)
