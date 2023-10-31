601bf20fcda93e5bb5d81cf695fb8c795db8125f
Deepak Kumar Bhardwaj <deepak@adeptmind.ai>
2023-10-31 09:35:23 +0530

    jellyfish int

--- scripts/move_stores_to_jellyfish.py
+++ scripts/move_stores_to_jellyfish.py
@@ -4,7 +4,9 @@ _path = "/Users/deepakkumar/Adeptmind/example.django22.mysite/scripts/jellyfish_
 
 
 def move_stores_to_jellyfish(_imap):
-    _map = yaml.safe_load(_path)
-    _map.update(_imap)
+    _map = yaml.safe_load(open(_path))
+    print("_map---", _map)
+    print("_imap---", _imap)
+    _map.update(eval(_imap))
     with open(_path, "w") as f:
         yaml.dump(_map, f)
